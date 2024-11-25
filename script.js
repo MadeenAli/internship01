72c0c32142ae63d7cefdf3051a971c303d6b290f
madeenali <madeenali19@gmail.com>
2024-11-25 17:09:32 +0500

    Product page completed

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ const countdownFunction = setInterval(() => {
     document.getElementById("seconds").innerHTML = "00";
   }
 }, 1000);
+
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
